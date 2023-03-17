d8841269d4843852f45d1c6a0c7a43e617543049
Joshua Slaughter <jslaughter.personal@gmail.com>
2023-03-17 13:25:59 -0500

    - Fixed festival alert pathing when installed into plugin folder

--- festival_notifier.py
+++ festival_notifier.py
@@ -6,8 +6,8 @@ class NotifierFunctor:
   
   def __call__(self,config,title,msg_text,priority,sound,url):
     festival_notifier_alert_on=config.get("festival_notifier_alert_on",[]);
-    if "*" is in festival_notifier_alert_on or sound is in festival_notifier_alert_on:
-        cmd="/opt/crows_nest/festival_alert/falert.sh '%s' '%s' '%d' '%s' '%s' " %(title,msg_text,priority,sound,url);
+    if ("*" in festival_notifier_alert_on) or (sound in festival_notifier_alert_on):
+        cmd="%s/festival_notifier/falert.sh '%s' '%s' '%d' '%s' '%s' " %(config['plugin_path'],title,msg_text,priority,sound,url);
         os.system(cmd);
     
 
