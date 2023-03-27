8d3b9ca614019c130f758a7e0a675826de439e84
gideonalpha <gideonalpha.dev@gmail.com>
2023-03-27 14:37:40 -0500

    - Added alert_type_name to calling convention

--- festival_notifier.py
+++ festival_notifier.py
@@ -4,9 +4,9 @@ class NotifierFunctor:
   def __init__(self,config):
     pass
   
-  def __call__(self,config,title,msg_text,priority,sound,url):
+  def __call__(self,config,title,msg_text,priority,alert_type_name,sound,url):
     festival_notifier_alert_on=config.get("festival_notifier_alert_on",[]);
-    if ("*" in festival_notifier_alert_on) or (sound in festival_notifier_alert_on):
+    if ("*" in festival_notifier_alert_on) or (alert_type_name in festival_notifier_alert_on):
         cmd="%s/festival_notifier/falert.sh '%s' '%s' '%d' '%s' '%s' " %(config['plugin_path'],title,msg_text,priority,sound,url);
         os.system(cmd);
     
